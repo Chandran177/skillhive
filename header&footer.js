074dc8c856acf31ddc5e2c8d56cc529004578abc
chandran <160822736+Chandran177@users.noreply.github.com>
2025-02-09 21:58:55 +0530

    chnages mede

--- header&footer.js
+++ header&footer.js
@@ -1,10 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(){
-    function loadHTML(selector, url){
-        fetch(url).then(res=> res.text()).then(data=>{
+
+document.addEventListener("DOMContentLoaded", function () {
+    function loadHTML(selector, url) {
+        fetch(url).then(res => res.text()).then(data => {
             document.querySelector(selector).innerHTML = data
-        }).catch(err=>console.error("error in data", err))
+        }).catch(err => console.error("error in data", err))
     }
 
-    loadHTML("header","/skillhive/header.html")
-    loadHTML("footer","/skillhive/footer.html")
-})
+    loadHTML("header", "/header.html");
+    loadHTML("footer", "/footer.html");
+
+    //  toggler = document.getElementById('bars');
+
+});
+
+
+
+function navtoggle() {
+    let navList = document.querySelector('.navlist');
+    navList.classList.toggle('hidden');
+}
+function clearnav() {
+
+    let navList = document.querySelector('.navlist');
+
+    navList.classList.remove('hidden');
+
+}
+
