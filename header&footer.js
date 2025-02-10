d59b1f43db5e78aa7d6625f9a46f7f212c854b15
Chandran177 <chandrantherko@gmail.com>
2025-02-10 10:50:41 +0530

    changes

--- header&footer.js
+++ header&footer.js
@@ -1,13 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    function loadHTML(selector, url) {
-        fetch(url).then(res => res.text()).then(data => {
-            document.querySelector(selector).innerHTML = data
-        }).catch(err => console.error("error in data", err))
-    }
+    try {
+        function loadHTML(selector, url) {
+            fetch(url).then(res => res.text()).then(data => {
+                document.querySelector(selector).innerHTML = data
+            }).catch(err => console.error("error in data", err))
+        }
 
-    loadHTML("header", "./header.html");
-    loadHTML("footer", "./footer.html");
+        loadHTML("header", "./header.html");
+        loadHTML("footer", "./footer.html");
+    } catch (e) {
+        console.log(e);
+    }
 
     //  toggler = document.getElementById('bars');
 
