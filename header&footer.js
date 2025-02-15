448597b650246e260751f9a6abe2b4703af0c27a
Chandran177 <chandrantherko@gmail.com>
2025-02-15 12:46:38 +0530

    changes

--- header&footer.js
+++ header&footer.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
             }).catch(err => console.error("error in data", err))
         }
 
-        loadHTML("header", "./header.html");
-        loadHTML("footer", "./footer.html");
+        loadHTML("header", "/header.html");
+        loadHTML("footer", "/footer.html");
     } catch (e) {
         console.log(e);
     }
