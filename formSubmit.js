c92a3b24ea9a3bfa034b9b7bb4d2663ca40655fc
Chandran177 <chandrantherko@gmail.com>
2025-02-25 14:31:31 +0530

    changes

--- formSubmit.js
+++ formSubmit.js
@@ -10,7 +10,8 @@ function handleFormSubmit(event) {
         email: formData.get("email"),
         phone: formData.get("phone"),
         education: formData.get("education"),
-        career : formData.get("career")
+        career: formData.get("career"),
+        course: formData.get("course"),
     };
  
     function dataClean() {
@@ -21,24 +22,17 @@ function handleFormSubmit(event) {
 
     const submitButton = form.querySelector("button[type='submit']");
     submitButton.disabled = true;
-
-    fetch('https://script.google.com/macros/s/AKfycbzyAFsOAMckLCBZKK_KuBxFrHdp6_BuCZWT5_CjkGfBVtZ3IAcyEj_m9ledWQqkQnSqAA/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbwPF2gGhWC8sWQmTppQijKmawLpQ84o7Kx_0VrrH2og6yb_HHYh7LZ1yBmJHz8gvh73/exec', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
         },
-        mode: 'no-cors'  // Add no-cors mode to bypass CORS restrictions
+        mode: 'no-cors'
     })
-    .then(response => {
-        dataClean();  
-        console.log(response);
-
-        // const modal = document.getElementById("modal");
-        // if (modal) {
-        //     modal.style.display = "none"; 
-        // }
-
+    .then(response => response.text())
+    .then(result => {
+        console.log('Success:', result);
         Swal.fire({
             icon: "success",
             title: "Form submitted successfully..",
@@ -47,18 +41,52 @@ function handleFormSubmit(event) {
         });
     })
     .catch(error => {
-        dataClean(); 
+        console.error('Error:', error);
         Swal.fire({
             icon: "error",
             title: "There was an error submitting the form!",
             showConfirmButton: false,
             timer: 1500
         });
-        console.error(error);
-    })
-    .finally(() => {
-        submitButton.disabled = false; 
     });
+    
+
+    // fetch('https://script.google.com/macros/s/AKfycbzyAFsOAMckLCBZKK_KuBxFrHdp6_BuCZWT5_CjkGfBVtZ3IAcyEj_m9ledWQqkQnSqAA/exec', {
+    //     method: 'POST',
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //      mode: 'no-cors'  // Add no-cors mode to bypass CORS restrictions
+    // })
+    // .then(response => {
+    //     dataClean();  
+    //     console.log(response);
+
+
+    //     Swal.fire({
+    //         icon: "success",
+    //         title: "Form submitted successfully..",
+    //         showConfirmButton: false,
+    //         timer: 1500
+    //     });
+    // })
+    // .catch(error => {
+    //     dataClean(); 
+    //     Swal.fire({
+    //         icon: "error",
+    //         title: "There was an error submitting the form!",
+    //         showConfirmButton: false,
+    //         timer: 1500
+    //     });
+    //     console.error(error);
+    // })
+    // .finally(() => {
+    //     submitButton.disabled = false; 
+    // });
 }
+
+
+
 
 document.getElementById("reserveForm").addEventListener("submit", handleFormSubmit);
